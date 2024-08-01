a5f94b5bf2a0fc260347790324eba30747ce5c4c
Rdxcj <qoefhsbfnx@gmail.com>
2024-08-01 15:42:59 +0530

    15:42:59

--- yt.py
+++ yt.py
@@ -66,4 +66,4 @@ a = l[-1]['url']
 print("AUDIO : ", a)
 print("VIDEO : ", v)
 
-os.system(f"ffmpeg -ss 00:5:00 -re -i '{v}' -ss 00:5:00 -re -i '{a}' -threads 4 -vcodec copy -b:v 9000k -c:a copy -preset ultrafast -tune zerolatency -f flv rtmp://a.rtmp.youtube.com/live2/7fgt-sqt1-09rj-aq34-69jy")
+os.system(f"ffmpeg -ss 00:10:00 -re -i '{v}' -ss 00:10:00 -re -i '{a}' -threads 4 -vcodec copy -b:v 9000k -c:a copy -preset ultrafast -tune zerolatency -f flv rtmp://a.rtmp.youtube.com/live2/7fgt-sqt1-09rj-aq34-69jy")
