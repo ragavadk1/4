d48abb7d4cc29b77f4d28c7d00bcac2453f52ca9
Rdxcj <qoefhsbfnx@gmail.com>
2024-08-14 09:59:31 +0530

    1723609771

--- yt.py
+++ yt.py
@@ -36,7 +36,7 @@ json_data = {
             'utcOffsetMinutes': 0,
         },
     },
-    'videoId': 'jTx5GetgC-M',
+    'videoId': 'gdmMZRH34uE',
     'playbackContext': {
         'contentPlaybackContext': {
             'html5Preference': 'HTML5_PREF_WANTS',
@@ -66,4 +66,4 @@ a = l[-1]['url']
 print("AUDIO : ", a)
 print("VIDEO : ", v)
 
-os.system(f"ffmpeg -ss 27:00:00 -to 28:00:00 -re -i '{v}' -ss 27:00:00 -to 28:00:00 -re -i '{a}' -threads 4 -vf \"format=yuv420p\" -c:v libx264 -b:v 9000k -c:a copy -preset ultrafast -tune zerolatency -f flv rtmp://a.rtmp.youtube.com/live2/zvmf-1yjp-jzek-01pw-b4js")
+os.system(f"ffmpeg -ss 00:00:00 -to 09:08:06 -re -i '{v}' -ss 00:00:00 -to 09:08:06 -re -i '{a}' -threads 4 -vf \"format=yuv420p\" -c:v libx264 -b:v 9000k -c:a copy -preset ultrafast -tune zerolatency -f flv rtmp://a.rtmp.youtube.com/live2/gkjq-gc2k-hbcc-3jwq-9pp6")
