4761d782aae6cf92eaac0de389d1616f028a96c8
Rdxcj <qoefhsbfnx@gmail.com>
2024-08-21 09:03:59 +0530

    @@11

--- Z.py
+++ Z.py
@@ -173,17 +173,17 @@ json_data = {
             ],
         },
     },
-    'videoId': 'kAmUDo_-5PI',
+    'videoId': 'jTx5GetgC-M',
     'playbackContext': {
         'contentPlaybackContext': {
-            'currentUrl': '/watch?v=kAmUDo_-5PI',
+            'currentUrl': '/watch?v=jTx5GetgC-M',
             'vis': 0,
             'splay': False,
             'autoCaptionsDefaultOn': False,
             'autonavState': 'STATE_NONE',
             'html5Preference': 'HTML5_PREF_WANTS',
             'signatureTimestamp': 19949,
-            'referer': 'https://www.youtube.com/watch?v=kAmUDo_-5PI',
+            'referer': 'https://www.youtube.com/watch?v=jTx5GetgC-M',
             'lactMilliseconds': '-1',
             'watchAmbientModeContext': {
                 'hasShownAmbientMode': True,
@@ -205,6 +205,7 @@ response = requests.post(
 pr = json.loads(response.text)["streamingData"]["hlsManifestUrl"]
 print(pr)
 
+#https://youtu.be/jTx5GetgC-M
 #os.system(f"ffmpeg -http_persistent 0 -re -i '{pr}' -threads 4 -vf \"format=yuv420p\" -c:v libx264 -g 48 -b:v 9000k -c:a copy -preset ultrafast -tune zerolatency -f flv rtmp://a.rtmp.youtube.com/live2/gkjq-gc2k-hbcc-3jwq-9pp6")
 
 os.system(f"ffmpeg -http_persistent 0 -ss 00:00:00 -re -i '{pr}' -threads 4 -vf \"format=yuv420p,drawtext=fontfile=_.ttf:text='FunnyBunny - YT':fontcolor=white:fontsize=18:box=0:boxcolor=black@0.5:boxborderw=15:x=w-tw:y=h-th\" -c:v libx264 -g 48 -b:v 9000k -c:a copy -preset ultrafast -tune zerolatency -f flv rtmp://a.rtmp.youtube.com/live2/u151-yfj5-6g19-c006-a0js")
